2ec1811d93caf5278995aff7cf7f73f96e0dbad2
Ryan Zebian <zebian93@gmail.com>
2019-05-19 20:33:12 -0700

    added two sample apps

---
+++ java/webapp11/src/main/java/com/example/webapp11/webapp11/Webapp11Application.java
@@ -0,0 +1,13 @@
+package com.example.webapp11.webapp11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp11Application.class, args);
+	}
+
+}
---
+++ java/webapp8/src/main/java/com/example/webapp18/webapp8/Webapp8Application.java
@@ -0,0 +1,13 @@
+package com.example.webapp18.webapp8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp8Application.class, args);
+	}
+
+}
